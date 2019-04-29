91a5cc1ec58150dd5418202e588a4077474d1565
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-29 22:24:17 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+  "hello" => "hi",
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "For" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  'at' => "@",
+  'and' => '&'
+  }
+end
+
+def word_substituter(tweets)
+  words = tweets.split
+  dictionary_word = dictionary.keys
+  words.collect do |string|
+      if dictionary_word.include?(string.downcase)
+          string = dictionary[string]
+      else
+        string
+      end
+end.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  arr.each do |string|
+    puts word_substituter(string)
+  end
+end
+
+
+def selective_tweet_shortener(tweets)
+  if tweets.length > 140 
+    return word_substituter(tweets)
+  else
+    tweets
+  end
+end
+
+ def shortened_tweet_truncator(tweets)
+    if tweets.length > 140
+      return tweets[0..136] + "..."
+    else
+      tweets
+    end
+end
